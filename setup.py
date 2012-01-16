55213b732cbd187515b15c0d1d1c4423b5e5348a
Mike Heffner <mikeh@fesnel.com>
2012-01-16 10:23:18 -0500

    Bump version to 0.4.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tablesnap',
-    version='0.4.1',
+    version='0.4.2',
     author='Jeremy Grosser',
     author_email='jeremy@synack.me',
     scripts=['tablesnap'],
