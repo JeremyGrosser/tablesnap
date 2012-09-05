eaf55154de79b29df25b084d61fac1db02c237b6
Jorge A Gallegos <kad@blegh.net>
2012-09-05 14:58:24 -0700

    This now requires boto >= 2.2
    There are some functions not available in previous versions of python
    boto, so I am bumping the version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='tablesnap',
-    version='0.4.3',
+    version='0.5',
     author='Jeremy Grosser',
     author_email='jeremy@synack.me',
     scripts=['tablesnap', 'tableslurp'],
     install_requires=[
         'pyinotify',
-        'boto',
+        'boto>=2.2',
     ]
 )
