1e68b4d945e16f3731b8e77fc42df7e09d94facc
Jeremy Grosser <jeremy@synack.me>
2017-01-07 14:29:18 -0800

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tablesnap',
-    version='0.7.3',
+    version='1.0.0',
     author='Jeremy Grosser',
     author_email='jeremy@synack.me',
     url='https://github.com/JeremyGrosser/tablesnap',
